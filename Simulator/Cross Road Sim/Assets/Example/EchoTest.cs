d11a16370c02a17f7914f925868f9bea6475eb25
Quget <eddymeivogel@gmail.com>
2018-10-08 12:36:53 +0200

    simulator
    simulator

---
+++ Simulator/Cross Road Sim/Assets/Example/EchoTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EchoTest : MonoBehaviour
+{
+    
+	// Use this for initialization
+	IEnumerator Start ()
+    {
+		WebSocket webSocket = new WebSocket(new Uri("ws://echo.websocket.org"));
+		yield return StartCoroutine(webSocket.Connect());
+		webSocket.SendString("Hi there");
+		int i=0;
+		while (true)
+		{
+			string reply = webSocket.RecvString();
+			if (reply != null)
+			{
+				Debug.Log ("Received: "+reply);
+				webSocket.SendString("Hi there"+i++);
+			}
+			if (webSocket.error != null)
+			{
+				Debug.LogError ("Error: "+webSocket.error);
+				break;
+			}
+			yield return 0;
+		}
+		webSocket.Close();
+	}
+}
